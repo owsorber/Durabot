8e62f5d99c6f90962196160fe2b59b6b441a9e2c
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-06-14 14:54:57 -0400

    Create RobotAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * This class manages a basic autonomous that can use the automatic methods from the Robot to make it perform a set of pre-programmed tasks.
+ *
+ * @author Ben Gerrard & Owen Sorber
+ * @date June 12th, 2018
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous(name = "Move to Position Automatic Mode 1", group = "Linear Opmode")
+public class RobotAuto extends LinearOpMode {
+    private Robot robot;
+    private Hardware hardware;
+
+    public void runOpMode() throws InterruptedException {
+        hardware = new Hardware(hardwareMap);
+        hardware.init();
+        robot = new Robot(hardware, gamepad1);
+
+        robot.moveToCoordinate(new Position(25, 25));
+        robot.moveToCoordinate(new Position());
+    }
+}
